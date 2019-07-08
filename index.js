47fe9ec12f8e561e449998f4c481d83e777b1dbd
Abhishek Venkat <abhisaivenkat@gmail.com>
2019-07-08 20:02:21 +0000

    highlight and basic movement

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000;
 
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/app.html"))
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('move', function(pos){
+      socket.broadcast.emit('move', pos)
+  })
+});
+
+
+http.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/index.js
+++
@@ -1,6 +0,0 @@
-var board2 = Chessboard('board', {
-    draggable: true,
-    sparePieces: true,
-    pieceTheme: 'img/{piece}.png',
-    position: "start"
-})
